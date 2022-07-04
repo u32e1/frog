f3c850911aff52d0ac9402ebc151f44ca0298bd5
l2ig <l@l5d.li>
2022-07-04 22:36:20 +0200

    feat: implement logic

--- src/index.js
+++ src/index.js
@@ -1,7 +1,68 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-exports.hello = void 0;
-function hello() {
-	return 'world';
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Frog = void 0;
+const fish_1 = require("@u32e1/fish");
+const node_fetch_1 = require("node-fetch");
+var LogType;
+(function (LogType) {
+    LogType[LogType["Info"] = 0] = "Info";
+    LogType[LogType["Warning"] = 1] = "Warning";
+    LogType[LogType["Error"] = 2] = "Error";
+})(LogType || (LogType = {}));
+const API_URL = 'https://v1.logs.api.u32e1.com/';
+class FrogArea {
+    constructor(config, areaName) {
+        this.config = config;
+        this.areaName = areaName;
+        this.fish = new fish_1.Fish(config.secret);
+    }
+    joinPieces(pieces) {
+        const formattedPieces = [];
+        for (const piece of pieces) {
+            if (typeof piece === 'object') {
+                formattedPieces.push(JSON.stringify(piece));
+                continue;
+            }
+            formattedPieces.push(String(piece));
+        }
+        return formattedPieces.join(' ');
+    }
+    async send({ area, content: _content, type, }) {
+        const content = this.fish.encrypt(_content);
+        await (0, node_fetch_1.default)(API_URL, {
+            method: 'post',
+            headers: {
+                'X-API-Key': this.config.apiKey,
+            },
+            body: JSON.stringify({ area, content, type }),
+        });
+    }
+    async info(...pieces) {
+        return await this.send({
+            area: this.areaName,
+            content: this.joinPieces(pieces),
+            type: LogType.Info,
+        });
+    }
 }
-exports.hello = hello;
+/**
+ * @example
+ * ```typescript
+ * const frog = new Frog({ secret: 'abc...', apiKey: 'abc...' });
+ * const log = new frog.Area('sign-up');
+ * ```
+ */
+class Frog {
+    constructor(config) {
+        this.config = config;
+        /**
+         * @example
+         * ```typescript
+         * const frog = new Frog(...);
+         * const log = new frog.Area('sign-up');
+         * ```
+         */
+        this.Area = FrogArea.bind(this, this.config);
+    }
+}
+exports.Frog = Frog;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,95 @@
-export function hello() {
-	return 'world';
+import { Fish } from '@u32e1/fish';
+import fetch from 'node-fetch';
+
+export interface FrogConfig {
+	/**
+	 * The key used to identify your project.
+	 * You can find this key in the project settings.
+	 */
+	apiKey: string;
+	/**
+	 * The key used for the AES-128 encryption.
+	 */
+	secret: string;
+}
+
+enum LogType {
+	Info = 0,
+	Warning = 1,
+	Error = 2,
+}
+
+const API_URL = 'https://v1.logs.api.u32e1.com/';
+
+class FrogArea {
+	private readonly fish: Fish;
+
+	constructor(private config: FrogConfig, private areaName: string | null) {
+		this.fish = new Fish(config.secret);
+	}
+
+	private joinPieces(pieces: unknown[]) {
+		const formattedPieces = [];
+
+		for (const piece of pieces) {
+			if (typeof piece === 'object') {
+				formattedPieces.push(JSON.stringify(piece));
+				continue;
+			}
+
+			formattedPieces.push(String(piece));
+		}
+
+		return formattedPieces.join(' ');
+	}
+
+	private async send({
+		area,
+		content: _content,
+		type,
+	}: {
+		area: string;
+		content: string;
+		type: LogType;
+	}) {
+		const content = this.fish.encrypt(_content);
+
+		await fetch(API_URL, {
+			method: 'post',
+			headers: {
+				'X-API-Key': this.config.apiKey,
+			},
+			body: JSON.stringify({ area, content, type }),
+		});
+	}
+
+	public async info(...pieces: unknown[]) {
+		return await this.send({
+			area: this.areaName,
+			content: this.joinPieces(pieces),
+			type: LogType.Info,
+		});
+	}
+}
+
+/**
+ * @example
+ * ```typescript
+ * const frog = new Frog({ secret: 'abc...', apiKey: 'abc...' });
+ * const log = new frog.Area('sign-up');
+ * ```
+ */
+export class Frog {
+	constructor(private config: FrogConfig) {}
+
+	/**
+	 * @example
+	 * ```typescript
+	 * const frog = new Frog(...);
+	 * const log = new frog.Area('sign-up');
+	 * ```
+	 */
+	public Area = FrogArea.bind(this, this.config) as {
+		new (...args: [ConstructorParameters<typeof FrogArea>[1]]);
+	};
 }
