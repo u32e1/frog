bad238ef7ce35b6e59818bbc1477fd17e78b81e7
Leonard Ginters <l@l5d.li>
2022-07-04 23:19:32 +0200

    feat: implement logic (#1)
    * feat: implement logic
    
    * fix: make `FrogArea` `areaName` arg required
    
    * refactor: change API url
    
    * refactor: improve tsc config
    
    * feat: add warning, error, export API_URL, LogType
    
    * test: add tests
    
    * refactor: improve typing
    
    * feat: fill in some information
    
    * chore: rebuild package-lock

--- README.md
+++ README.md
@@ -1,9 +1,25 @@
-# CHANGE_ME
+# @u32e1/frog
 
-CHANGE_ME
+The u32e1 SDK for Node.js.
 
 ## Usage
 
 ```typescript
+/**
+ * apiKey: string;
+ * The key used to identify your project.
+ * You can find this key in the project settings.
+ *
+ * secret: string;
+ * The key used for the AES-128 encryption.
+ */
+const frog = new Frog({ apiKey: 'abc...', secret: 'abc...' });
+const log = new frog.Area('sign_up');
 
+try {
+	await insertUserIntoDatabase(user);
+	log.info('A new user signed up! Their email is', user.email);
+} catch (error) {
+	log.error('An error occured :(', error);
+}
 ```
--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,97 @@
-import { test, assert } from 'vitest';
-import { hello } from './index';
+import { test, vi, expect, assert } from 'vitest';
+import * as fetchModule from 'node-fetch';
+import { Frog, LogType, API_URL } from './index';
 
-test('hello', () => {
-	assert(hello(), 'world');
+vi.mock('node-fetch', () => {
+	return {
+		default: vi.fn(() => null),
+	};
+});
+const fetchSpy = vi.spyOn(fetchModule, 'default');
+
+const API_KEY = 'abc';
+
+const frog = new Frog({ apiKey: API_KEY, secret: 'abc' });
+
+test('info', () => {
+	const log = new frog.Area('test');
+	log.info('test');
+
+	expect(fetchSpy).toHaveBeenCalledWith(API_URL, {
+		method: 'post',
+		headers: {
+			'X-API-Key': API_KEY,
+		},
+		body: JSON.stringify({
+			area: 'test',
+			content: 'b9a8e4c1',
+			type: LogType.Info,
+		}),
+	});
+});
+
+test('warning', () => {
+	const log = new frog.Area('test');
+	log.warning('test');
+
+	expect(fetchSpy).toHaveBeenCalledWith(API_URL, {
+		method: 'post',
+		headers: {
+			'X-API-Key': API_KEY,
+		},
+		body: JSON.stringify({
+			area: 'test',
+			content: 'b9a8e4c1',
+			type: LogType.Warning,
+		}),
+	});
+});
+
+test('error', () => {
+	const log = new frog.Area('test');
+	log.error('test');
+
+	expect(fetchSpy).toHaveBeenCalledWith(API_URL, {
+		method: 'post',
+		headers: {
+			'X-API-Key': API_KEY,
+		},
+		body: JSON.stringify({
+			area: 'test',
+			content: 'b9a8e4c1',
+			type: LogType.Error,
+		}),
+	});
+});
+
+test('different areas', () => {
+	const log = new frog.Area('test');
+	log.info('test');
+
+	expect(fetchSpy).toHaveBeenCalledWith(API_URL, {
+		method: 'post',
+		headers: {
+			'X-API-Key': 'abc',
+		},
+		body: JSON.stringify({
+			area: 'test',
+			content: 'b9a8e4c1',
+			type: LogType.Info,
+		}),
+	});
+
+	const log2 = new frog.Area('test2');
+	log2.info('test');
+
+	expect(fetchSpy).toHaveBeenCalledWith(API_URL, {
+		method: 'post',
+		headers: {
+			'X-API-Key': 'abc',
+		},
+		body: JSON.stringify({
+			area: 'test2',
+			content: 'b9a8e4c1',
+			type: LogType.Info,
+		}),
+	});
 });
